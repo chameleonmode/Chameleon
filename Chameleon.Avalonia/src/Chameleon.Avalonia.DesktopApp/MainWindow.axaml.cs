cce100944c001f15f2cadc31980beb65489de60f
edads <elimdadia@gmail.com>
2024-02-21 13:58:20 +0200

    -added start avalonia project

---
+++ Chameleon.Avalonia/src/Chameleon.Avalonia.DesktopApp/MainWindow.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace Chameleon.Avalonia.DesktopApp;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
