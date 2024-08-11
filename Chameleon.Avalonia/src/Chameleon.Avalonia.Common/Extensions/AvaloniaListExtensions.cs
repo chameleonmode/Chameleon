a7d99feabd67ad0ae8aac35546bbba9120f2c19e
ezexe <elimdadia@gmail.com>
2024-08-11 12:50:12 +0300

    UserProfilePersonBindable and UserProfileAdditionalDataService Datetime possible crashes

--- Chameleon.Avalonia/src/Chameleon.Avalonia.Common/Extensions/AvaloniaListExtensions.cs
+++ Chameleon.Avalonia/src/Chameleon.Avalonia.Common/Extensions/AvaloniaListExtensions.cs
@@ -6,6 +6,6 @@ public static class AvaloniaListExtensions
     public static async void AddNewRangeAsync<T>(this AvaloniaList<T> sels, Task<IEnumerable<T>> values)
     {
         sels.Clear();
-        sels.AddRange(await values);
+        sels.AddRange(await values.ConfigureAwait(false));
     }
 }
