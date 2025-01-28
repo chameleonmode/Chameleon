517903b21b81c99f3f13a2785c65a75e20fb0a65
Jack Mutobu <jmutobu@outlook.com>
2025-01-28 13:25:46 -0800

    Update Viewmodels and cache

---
+++ Chameleon.app.Avalonia/Features/ProfilesAndFolders/Profiles/Identity/ViewModels/UPPersonViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Chameleon.lib.Common.Constants;
+using Chameleon.lib.CommunityToolkit.MvvM;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Chameleon.app.Avalonia.Features.ProfilesAndFolders.Profiles.Identity.ViewModels;
+public partial class UPPersonViewModel: ObservableObjectBase {
+
+	[ObservableProperty]
+	public int id;
+
+	[ObservableProperty]
+	public int? profileId;
+
+	[ObservableProperty]
+	private string? firstName;
+
+	[ObservableProperty]
+	private string? lastName;
+
+	[ObservableProperty]
+	private string? middleName;
+
+	[ObservableProperty]
+	private string? jobTitle;
+
+	[ObservableProperty]
+	private string? phoneNumber;
+
+	[ObservableProperty]
+	private string? email;
+
+	[ObservableProperty]
+	private string? birthPlace;
+
+	[ObservableProperty]
+	private string? notes;
+
+	[ObservableProperty]
+	private DateTime birthDate = DateTimeOffset.Now.AddYears(-20).DateTime;
+
+	public DateTimeOffset BirthDateOffset => new(BirthDate);
+
+	[ObservableProperty]
+	private Enums.GenderType gender = Enums.GenderType.Female;
+	public string Gendertext => Gender.ToString();
+}
