d09757dfa0acb123724f7a1e45d5bba7f7a1b491
Jack Mutobu <jmutobu@outlook.com>
2025-02-04 09:53:28 -0800

    Start working on the view

---
+++ Chameleon.app.Avalonia/Features/ProfilesAndFolders/Search/ByTags/View.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Chameleon.app.Avalonia.Features.ProfilesAndFolders.Search.ByTags;
+
+public partial class View : UserControl
+{
+    public View()
+    {
+        InitializeComponent();
+    }
+}
