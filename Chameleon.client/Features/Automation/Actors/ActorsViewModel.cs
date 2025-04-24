4377ff75a3f2f32f1ac3b264ff5ada9a19d5a879
ezexe@mac <elimdadia@gmail.com>
2025-04-24 13:23:40 +0300

    Refactor ActorsView and ActorsViewModel to enhance item display and command handling

--- Chameleon.client/Features/Automation/Actors/ActorsViewModel.cs
+++ Chameleon.client/Features/Automation/Actors/ActorsViewModel.cs
@@ -1,13 +1,21 @@
 using System.Collections.ObjectModel;
 using Chameleon.AIR.Actors.Models;
 using Chameleon.lib.CommunityToolkit.MvvM;
+using CommunityToolkit.Mvvm.Input;
 using RedditActor = Chameleon.AIR.Actors.Models.Reddit.Actor;
 
 namespace Chameleon.client.Features.Automation.Actors;
 
 public partial class ActorsViewModel : ViewModelObjectBase {
-  public ObservableCollection<IActor> Actors = [new RedditActor()];
+  public ObservableCollection<IActor> Actors { get; set; } = [new RedditActor()];
   public ActorsViewModel() : base("Mr. AI-Agent") {
 
+  }
+
+  [RelayCommand]
+  public void Open(IActor actor) {
+    if (actor is RedditActor redditActor) {
+      
+    }
   }
 }
