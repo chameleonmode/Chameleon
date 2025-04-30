54b7e76265a3a8637712182abca1a571583e3c82
ezexe@mac <elimdadia@gmail.com>
2025-04-30 17:32:38 +0300

    Refactor Actors feature: streamline ViewModels by removing deprecated classes, enhance data handling, and improve structure for better maintainability.

--- Chameleon.client/Features/Automation/Actors/ViewModels/ArgsViewModel.cs
+++ Chameleon.client/Features/Automation/Actors/ViewModels/ArgsViewModel.cs
@@ -1,9 +1,10 @@
-﻿using Chameleon.AIR.Actors.Models.Reddit;
+﻿using Chameleon.AIR.Actors.Models;
+using Chameleon.AIR.Actors.Models.Reddit;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Chameleon.client.Features.Automation.Actors.ViewModels;
 public partial class ArgsViewModel : ObservableObject {
-	[ObservableProperty] string? _search;
+	[ObservableProperty] string _search;
 	[ObservableProperty] Scope _selectedScope;
 	[ObservableProperty] Sort _selectedSort;
 	[ObservableProperty] Filter _selectedFilter;
@@ -12,22 +13,24 @@ public partial class ArgsViewModel : ObservableObject {
 	public IEnumerable<Sort> AvailableSorts { get; } = Enum.GetValues<Sort>();
 	public IEnumerable<Filter> AvailableFilters { get; } = Enum.GetValues<Filter>();
 
-	public ArgsViewModel(IDictionary<string, string> sourceArgs) {
-		Search = sourceArgs.TryGetValue("Search", out var search) ? search?.ToString() : null;
+	public ArgsViewModel(DictionaryArgs sourceArgs) {
+		Search =
+			sourceArgs.TryGetValue("Search", out var search)
+			? search?.ToString() ?? "" : "";
 		SelectedScope =
-			sourceArgs.TryGetValue("Scope", out var scope) && Enum.TryParse<Scope>(scope, out var scopeEnum)
+			sourceArgs.TryGetValue("Scope", out var scope) && Enum.TryParse<Scope>(scope?.ToString(), out var scopeEnum)
 			? scopeEnum : Scope.Posts;
 		SelectedSort =
-			sourceArgs.TryGetValue("Sort", out var sort) && Enum.TryParse<Sort>(sort, out var sortEnum)
+			sourceArgs.TryGetValue("Sort", out var sort) && Enum.TryParse<Sort>(sort?.ToString(), out var sortEnum)
 			? sortEnum : Sort.Relevance;
 		SelectedFilter =
-			sourceArgs.TryGetValue("Filter", out var filter) && Enum.TryParse<Filter>(filter, out var filterEnum)
+			sourceArgs.TryGetValue("Filter", out var filter) && Enum.TryParse<Filter>(filter?.ToString(), out var filterEnum)
 			? filterEnum : Filter.All;
 	}
 
-	public Dictionary<string, string> ToDictionary() {
-		return new Dictionary<string, string> {
-			["search"] = Search ?? "",
+	public DictionaryArgs ToDictionary() {
+		return new DictionaryArgs {
+			["search"] = Search.Contains(',') ? Search.Split(",").Select(x => x.Trim()) : [Search.Trim()],
 			["scope"] = SelectedScope.ToString(),
 			["sort"] = SelectedSort.ToString(),
 			["filter"] = SelectedFilter.ToString()
--- Chameleon.client/Features/Automation/Actors/ViewModels/SettingsViewModel.cs
+++ Chameleon.client/Features/Automation/Actors/ViewModels/SettingsViewModel.cs
@@ -2,18 +2,9 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Chameleon.client.Features.Automation.Actors.ViewModels;
-public partial class SettingsViewModel: ObservableObject {
+public partial class SettingsViewModel(Settings source) : ObservableObject {
+	[ObservableProperty] Start start = source.Start;
+	[ObservableProperty] Timeouts timeouts = source.Timeouts;
 
-	[ObservableProperty] StartViewModel start = new();
-	[ObservableProperty] TimeoutsViewModel timeouts = new();
-	[ObservableProperty] RandoViewModel rando = new();
-	[ObservableProperty] RandoViewModel iterations = new();
-
-	public SettingsViewModel(Settings source) {
-		Start = new StartViewModel(source.Start);
-		Timeouts = new TimeoutsViewModel(source.Timeouts);
-		Rando = new RandoViewModel(source.Rando);
-		Iterations = new RandoViewModel(source.Iterations);
-	}
-	public Settings ToRecord() => new(Start.ToRecord(), Timeouts.ToRecord(), Rando.ToRecord(), Iterations.ToRecord());
+	public Settings ToRecord() => new(Start, Timeouts);
 }
