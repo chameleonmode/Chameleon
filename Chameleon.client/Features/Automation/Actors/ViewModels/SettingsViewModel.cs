6a8e06b032026b0a54b4db87695f5644868a7256
ezexe@mac <elimdadia@gmail.com>
2025-05-01 12:36:21 +0300

    Refactor UI styles and templates: streamline ListBox and ControlTheme definitions for improved organization and maintainability.

--- Chameleon.client/Features/Automation/Actors/ViewModels/SettingsViewModel.cs
+++ Chameleon.client/Features/Automation/Actors/ViewModels/SettingsViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using Chameleon.AIR.Actors.Models;
+using Chameleon.lib.Util;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Chameleon.client.Features.Automation.Actors.ViewModels;
+
 public partial class SettingsViewModel(Settings source) : ObservableObject {
 	[ObservableProperty] Start start = source.Start;
 	[ObservableProperty] Timeouts timeouts = source.Timeouts;
 
-	public Settings ToRecord() => new(Start, Timeouts);
+	public Settings ToRecord() {
+		Start.Urls = Start.Url?.Split('\n').Where(x => x.IsNot()).Select(x => x.Trim());
+		return new(Start, Timeouts);
+	}
 }
