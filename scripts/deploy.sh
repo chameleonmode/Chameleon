5a33fe0eadaf2c521639c18df748c1c8a3c0a5d9
ezexe@mac <elimdadia@gmail.com>
2025-06-12 07:18:49 +0300

    Refactor deploy.sh to remove redundant cd commands and improve script clarity

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,12 @@
-cd /Users/dev/src/Chameleon/scripts
+#!/bin/bash
+# cd /Users/dev/src/Chameleon/scripts
+# bash deploy.sh 
+
 bash macnoto.sh
-cd /Users/dev/src/Chameleon/scripts
 bash macpload.sh
 echo "[Info] (mac/status):\n\t bash macstatus.sh <notarization UUID>"
 echo "bash macstatus.sh" | pbcopy
 
-cd /Users/dev/src/Chameleon/scripts
 bash winbuild.sh
-cd /Users/dev/src/Chameleon/scripts
 bash winpload.sh
-cd /Users/dev/src/Chameleon/scripts
 bash macship.sh
